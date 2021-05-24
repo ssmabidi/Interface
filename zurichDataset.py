151f6160f1eae52bab74dc9c0c8b042ceefda9d1
Shahamat Mehdi <syed.shahamat@gmail.com>
2021-05-24 17:06:55 +0200

    Update Zurich Dataset

--- zurichDataset.py
+++ zurichDataset.py
@@ -1,20 +1,9 @@
+from dataSetInterface import DataSetInterface
 import numpy as np
 import os
 from PIL import Image
 
-class Interface:
-
-    # Default values for dataSets taken from related papers
-    # Values for Zurich dataSet. Paper link: http://rpg.ifi.uzh.ch/docs/IJRR17_Majdik.pdf
-    camera = {
-        'zurich': {
-            'type': "PinHole",
-            'width': 1920,
-            'height': 1080,
-            'fps': 30,
-            'RGB': 1
-        }
-    }
+class ZurichDataset(DataSetInterface):
 
     # TODO: Currently these values are copied from EuRoC dataset. Need to update for zurich
     orb = {
@@ -50,79 +39,88 @@ class Interface:
               0, 0, 1, 0.03227,
                0.0, 0.0, 0.0, 1.0]
     }
-    paths = {
-        'zurich': {
+
+    def __init__(self, args: list):
+        # Default values for dataSets taken from related papers
+        # Values for Zurich dataSet. Paper link: http://rpg.ifi.uzh.ch/docs/IJRR17_Majdik.pdf
+        self.camera = {
+            'type': "PinHole",
+            'width': 1920,
+            'height': 1080,
+            'fps': 30,
+            'RGB': 1
+        }
+        self.paths = {
             'calibrationDataPath': '/calibration_data.npz',
             'logFilesPath': '/Log Files',
             'imageFilesPath': '/MAV Images'
         }
-    }
-
-    def __init__(self, args: list):
-        print("Interface")
         self.dataPath = args["dataPath"]
-        self.dataSet = args["dataSet"].lower()
-        self.imageNames = sorted(os.listdir(self.dataPath + self.paths[self.dataSet]['imageFilesPath']))
+        self.imageNames = sorted(os.listdir(self.dataPath + self.paths['imageFilesPath']))
         self.imageNameIndex = 0
         self.imageIndex = 0
         # unzipped_file = zipfile.ZipFile("sample.zip", "r")
-    # self.name = name
-    # self.age = age
-
+    
     # For reading the Intrinsic Matrix lecture from Kyle Simek is used which is available at http://ksimek.github.io/2013/08/13/intrinsic/
-    def get_cameraParams(self):
-        calibrationdata = np.load(self.dataPath + self.paths[self.dataSet]['calibrationDataPath'])
+    def get_cameraParams(self) -> dict:
+        calibrationdata = np.load(self.dataPath + self.paths['calibrationDataPath'])
         mtx = calibrationdata['intrinsic_matrix']
         dist = calibrationdata['distCoeff'][0]
-        print(self.camera['zurich']['type'])
+        print(self.camera['type'])
 
-        self.camera[self.dataSet]['fx'] = mtx[0][0]
-        self.camera[self.dataSet]['s'] = mtx[0][1]
-        self.camera[self.dataSet]['fy'] = mtx[1][1]
-        self.camera[self.dataSet]['cx'] = mtx[0][2]
-        self.camera[self.dataSet]['cy'] = mtx[1][2]
+        self.camera['fx'] = mtx[0][0]
+        self.camera['s'] = mtx[0][1]
+        self.camera['fy'] = mtx[1][1]
+        self.camera['cx'] = mtx[0][2]
+        self.camera['cy'] = mtx[1][2]
 
-        self.camera[self.dataSet]['k1'] = dist[0]
-        self.camera[self.dataSet]['k2'] = dist[1]
-        self.camera[self.dataSet]['p1'] = dist[2]
-        self.camera[self.dataSet]['p2'] = dist[3]
-        self.camera[self.dataSet]['k3'] = dist[4]
+        self.camera['k1'] = dist[0]
+        self.camera['k2'] = dist[1]
+        self.camera['p1'] = dist[2]
+        self.camera['p2'] = dist[3]
+        self.camera['k3'] = dist[4]
 
         calibrationdata.close()
 
-        return self.camera[self.dataSet]
+        return self.camera
 
     # TODO: Implement details after more research
-    def getOrbParams(self):
-        return self.orb[self.dataSet]
+    def getOrbParams(self) -> dict:
+        return self.orb
 
     # TODO: Implement details after more research
-    def getViewerParams(self):
-        return self.Viewer[self.dataSet]
+    def getViewerParams(self) -> dict:
+        return self.Viewer
 
 
-    def getImageNames(self):
+    def getImageNames(self) -> list:
         return self.imageNames
 
-    def getNextImageName(self):
+    def getNextImageName(self) -> str:
         retFileName = None
         if(self.imageNameIndex < len(self.imageNames)):
             retFileName = self.imageNames[self.imageNameIndex]
             self.imageNameIndex += 1
         return retFileName
 
-    def getImageAtIndex(self, i: int):
+    def getImageAtIndex(self, i: int) -> Image:
         """Returns Image at index i. If i is greater than total number of images returns None."""
-        # print(self.dataPath + self.paths[self.dataSet]['imageFilesPath'] + "/" + self.imageNames[i])
+        # print(self.dataPath + self.paths['imageFilesPath'] + "/" + self.imageNames[i])
         if(i >= 0 and i < len(self.imageNames)):
-            img = Image.open(self.dataPath + self.paths[self.dataSet]['imageFilesPath'] + "/" + self.imageNames[i])
+            img = Image.open(self.dataPath + self.paths['imageFilesPath'] + "/" + self.imageNames[i])
             return img
         else:
             return None
 
-    def getNextImage(self):
+    def getNextImage(self) -> Image:
         retImg = None
         if(self.imageIndex < len(self.imageNames)):
-            retImg = Image.open(self.dataPath + self.paths[self.dataSet]['imageFilesPath'] + "/" + self.imageNames[self.imageIndex])
+            retImg = Image.open(self.dataPath + self.paths['imageFilesPath'] + "/" + self.imageNames[self.imageIndex])
             self.imageIndex += 1
         return retImg
+
+    def setImageIndex(self, i: int = 0):
+        '''Sets image index to i. Default value is 0.'''
+        self.imageIndex = i
+
+        
