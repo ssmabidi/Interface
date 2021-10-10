8995f8bda1cb5f2cb51d53e2bb579367e75cc88a
Shahamat Mehdi <syed.shahamat@gmail.com>
2021-10-10 12:58:04 +0200

    Updated callbacks - Dataset Page callbacks complete

--- webPage/callbacks.py
+++ webPage/callbacks.py
@@ -1,7 +1,3 @@
-import base64
-import os
-from urllib.parse import quote as urlquote
-import cv2
 import dash
 from app import app
 
@@ -18,12 +14,6 @@ import numpy as np
 import globals as g_var
 
 
-import datetime
-import io
-
-import dash_table
-
-import pandas as pd
 
 
 ####################################################################################################
@@ -32,7 +22,8 @@ import pandas as pd
 # Callbacks for Datasets and Algorithms Page
 ####################################################################################################
 ####################################################################################################
-####################################################################################################
+############################        
+########################################################################
 
 
 ####################################################################################################
@@ -93,7 +84,7 @@ def applyAlgo(algo, apply_click):
 ####################################################################################################
 
 ####################################################################################################
-# 005 - Select Dataset
+# 001 - Select Dataset && # 002 - #006 - View Images
 ####################################################################################################
 @app.callback(
     [Output('config-header', 'children')
@@ -110,19 +101,37 @@ def applyAlgo(algo, apply_click):
     , Output('orb-nLevels', 'value')
     , Output('orb-iniThFast', 'value')
     , Output('orb-minThFast', 'value')
-    # , Output('dataset-training-percent', 'value')
-    # , Output('dataset-training-percent', 'value')
-    # , Output('dataset-training-percent', 'value')
-    # , Output('dataset-training-percent', 'value')
-    # , Output('dataset-training-percent', 'value')
-    # , Output('dataset-training-percent', 'value')
-    # , Output('dataset-training-percent', 'value')
-    # , Output('dataset-training-percent', 'value')
-    # , Output('dataset-training-percent', 'value')
-    # , Output('dataset-training-percent', 'value')
+    , Output('viewer-kfs', 'value')
+    , Output('viewer-kflw', 'value')
+    , Output('viewer-glw', 'value')
+    , Output('viewer-ps', 'value')
+    , Output('viewer-cs', 'value')
+    , Output('viewer-clw', 'value')
+    , Output('viewer-cpX', 'value')
+    , Output('viewer-vpY', 'value')
+    , Output('viewer-vpZ', 'value')
+    , Output('viewer-cpF', 'value')
+    , Output('dataset-view-image', 'figure')
+    , Output('dataset-type-dropdown', 'value')
+    , Output('img-range', 'max')
+    , Output("notification-toast", "children")
+    , Output("notification-toast", "is_open")
+    , Output("basic-info-toast", "children")
+    , Output("basic-info-toast", "is_open")
     ],
-    [Input('dataset-config-dropdown', 'value')])
-def selectDatasetConfig(dataset):
+    [Input('dataset-config-dropdown', 'value')
+    , Input('img-first', 'n_clicks')
+    , Input('img-prev', 'n_clicks')
+    , Input('img-next', 'n_clicks')
+    , Input('img-last', 'n_clicks')
+    , Input('img-range', 'value')
+    ]
+    )
+def selectDatasetConfig(dataset, btnFirst, btnPrev, btnNext, btnLast, rangeVal):
+
+    ctx = dash.callback_context
+    triggerCause = ctx.triggered[0]['prop_id'].split('.')[0]
+
     if(dataset == None):
         raise PreventUpdate
     if(dataset == 'zurich'):
@@ -133,28 +142,69 @@ def selectDatasetConfig(dataset):
         g_var.datasetInstance = None
 
     header = 'Select a Dataset'
+
+    notificationText = "Dataset Loaded"
+    notificationAlert = True
+    basicInfoText = dash.no_update
+    basicInfoAlert = False
+
+    fig = None
     if(g_var.datasetInstance != None):
-        # g_var.availableDatasets
         header = dataset.capitalize() + ' Configurations'
         total_images = g_var.datasetInstance.getTotalImages()
         dataset_path = g_var.datasetInstance.getDatasetPath()
         traingDataPercent = g_var.datasetInstance.getTrainingPercent()
         cameraConfig = g_var.datasetInstance.get_cameraParams()
         orbConfig = g_var.datasetInstance.getOrbParams()
-        traingDataPercent = g_var.datasetInstance.getTrainingPercent()
-        traingDataPercent = g_var.datasetInstance.getTrainingPercent()
-        traingDataPercent = g_var.datasetInstance.getTrainingPercent()
-        traingDataPercent = g_var.datasetInstance.getTrainingPercent()
-        traingDataPercent = g_var.datasetInstance.getTrainingPercent()
-        traingDataPercent = g_var.datasetInstance.getTrainingPercent()
-        traingDataPercent = g_var.datasetInstance.getTrainingPercent()
-        traingDataPercent = g_var.datasetInstance.getTrainingPercent()
-        print(orbConfig)
+        viewerConfig = g_var.datasetInstance.getViewerParams()
+        datasetType = g_var.datasetInstance.getDatasetType()
+        if(triggerCause == 'dataset-config-dropdown'):
+            fig = go.Figure(px.imshow(g_var.datasetInstance.getNextImage()))
+        else:
+            notificationText = dash.no_update
+            notificationAlert = False
+        if(triggerCause == 'img-next'):
+            fig = go.Figure(px.imshow(g_var.datasetInstance.getNextImage()))
+            basicInfoText = "Next Image Loaded"
+            basicInfoAlert = True
+        if(triggerCause == 'img-first'):
+            basicInfoText = "First Image Loaded"
+            basicInfoAlert = True
+            fig = go.Figure(px.imshow(g_var.datasetInstance.getImageAtIndex(0)))
+        if(triggerCause == 'img-last'):
+            basicInfoText = "Last Image Loaded"
+            basicInfoAlert = True
+            fig = go.Figure(px.imshow(g_var.datasetInstance.getImageAtIndex(total_images - 1)))
+        if(triggerCause == 'img-prev'):
+            basicInfoText = "Previous Image Loaded"
+            basicInfoAlert = True
+            fig = go.Figure(px.imshow(g_var.datasetInstance.getPrevImage()))
+        if(triggerCause == 'img-range'):
+            basicInfoText = "Image Number " + str(rangeVal) + " loaded"
+            basicInfoAlert = True
+            fig = go.Figure(px.imshow(g_var.datasetInstance.getImageAtIndex(rangeVal)))
+
     return [header, total_images, dataset_path, traingDataPercent, cameraConfig['type'], cameraConfig['width'], cameraConfig['height'], 
      cameraConfig['fps'], cameraConfig['RGB'], orbConfig['nFeatures'], orbConfig['scaleFactor'], orbConfig['nLevels'], orbConfig['iniThFAST'],
-     orbConfig['minThFAST'], 
-    # traingDataPercent, traingDataPercent, traingDataPercent, traingDataPercent, traingDataPercent, traingDataPercent, traingDataPercent, traingDataPercent, traingDataPercent, traingDataPercent, traingDataPercent, traingDataPercent, traingDataPercent, traingDataPercent, traingDataPercent, traingDataPercent, traingDataPercent, traingDataPercent, traingDataPercent
+     orbConfig['minThFAST'], viewerConfig['KeyFrameSize'], viewerConfig['KeyFrameLineWidth'], viewerConfig['GraphLineWidth'], viewerConfig['PointSize'],
+     viewerConfig['CameraSize'], viewerConfig['CameraLineWidth'], viewerConfig['ViewpointX'], viewerConfig['ViewpointY'], viewerConfig['ViewpointZ'],
+     viewerConfig['ViewpointF'], fig, datasetType, total_images, notificationText, notificationAlert, basicInfoText, basicInfoAlert
     ]
+
+####################################################################################################
+# 007 - set Training Data Percent
+####################################################################################################
+@app.callback(
+    [ Output("basic-config-toast", "children")
+    , Output("basic-config-toast", "is_open")
+    ],
+    [ Input('dataset-training-percent', 'value')
+    ]
+    )
+def setTrainingPercent(dsTrainVal):
+    g_var.datasetInstance.setTrainingPercent(dsTrainVal)
+    notificationText = "Training Data Percentage Updated Successfully"
+    return [notificationText, True]
 
 
 ####################################################################################################
@@ -166,10 +216,8 @@ def selectDatasetConfig(dataset):
 ####################################################################################################
 
 ####################################################################################################
-# 005 - Upload File
+# 001 - Upload Config File
 ####################################################################################################
-
-
 @app.callback(
     Output('callback-config-output', 'children'),
     [Input('upload-config-file', 'isCompleted')],
@@ -181,11 +229,14 @@ def callback_on_completion(iscompleted, filenames, upload_id):
         return
 
     if filenames is not None:
+        g_var.algoInstance.set_config_file(g_var.upload_dir+"/"+upload_id+"/"+filenames[0])
         return html.Div([filenames[0] + "uploaded successfully." ])
 
     return html.Div("No Files Uploaded Yet!")
 
-
+####################################################################################################
+# 002 - Upload Data File
+####################################################################################################
 @app.callback(
     Output('callback-data-output', 'children'),
     [Input('upload-data-file', 'isCompleted')],
@@ -197,21 +248,26 @@ def callback_on_completion(iscompleted, filenames, upload_id):
         return
 
     if filenames is not None:
+        g_var.algoInstance.set_data_file(g_var.upload_dir+"/"+upload_id+"/"+filenames[0])
         return html.Div([filenames[0] + "uploaded successfully." ])
 
     return html.Div("No Files Uploaded Yet!")
 
+####################################################################################################
+# 003 - Upload Weights File
+####################################################################################################
 @app.callback(
     Output('callback-weights-output', 'children'),
-    [Input('upload-weights-test-file', 'isCompleted')],
-    [State('upload-weights-test-file', 'fileNames'),
-     State('upload-weights-test-file', 'upload_id')],
+    [Input('upload-weights-file', 'isCompleted')],
+    [State('upload-weights-file', 'fileNames'),
+     State('upload-weights-file', 'upload_id')],
 )
 def callback_on_completion(iscompleted, filenames, upload_id):
     if not iscompleted:
         return
 
     if filenames is not None:
+        g_var.algoInstance.set_weights_file(g_var.upload_dir+"/"+upload_id+"/"+filenames[0])
         return html.Div([filenames[0] + "uploaded successfully." ])
 
     return html.Div("No Files Uploaded Yet!")
